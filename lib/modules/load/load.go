c95b4662df65ff29f6d330883881e71f1dd87c32
Stefan Tatschner <rumpelsepp@sevenbyte.org>
2016-11-21 21:04:31 +0100

    Add support for UrgentLoad in module load

--- lib/modules/load/load.go
+++ lib/modules/load/load.go
@@ -4,6 +4,8 @@ import (
 	"bytes"
 	"fmt"
 	"io/ioutil"
+	"runtime"
+	"strconv"
 	"strings"
 	"text/template"
 	"time"
@@ -14,9 +16,10 @@ import (
 )
 
 const (
-	name          = "load"
-	moduleName    = "i3gostatus.modules." + name
-	defaultFormat = "{{.Avg1}} {{.Avg5}} {{.Avg15}}"
+	name               = "load"
+	moduleName         = "i3gostatus.modules." + name
+	defaultFormat      = "{{.Avg1}} {{.Avg5}} {{.Avg15}}"
+	defaultUrgentColor = "#ff0000"
 )
 
 type Load struct {
@@ -41,22 +44,33 @@ func getLoad() Load {
 
 type Config struct {
 	model.BaseConfig
+	UrgentLoad  float64
+	UrgentColor string
 }
 
 func (c *Config) ParseConfig(configTree *toml.TomlTree) {
 	c.BaseConfig.Parse(name, configTree)
 	c.Format = config.GetString(configTree, name+".format", defaultFormat)
+	c.UrgentLoad = config.GetFloat64(configTree, name+".urgent_load", float64(runtime.NumCPU()))
+	c.UrgentColor = config.GetString(configTree, name+".urgent_color", defaultUrgentColor)
 }
 
 func (c *Config) Run(args *model.ModuleArgs) {
-	outputBlock := model.NewBlock(moduleName, c.BaseConfig, args.Index)
-	t := template.Must(template.New("load").Parse(c.Format))
 	var outStr string
+	t := template.Must(template.New("load").Parse(c.Format))
 
 	for range time.NewTicker(c.Period).C {
+		outputBlock := model.NewBlock(moduleName, c.BaseConfig, args.Index)
 		buf := bytes.NewBufferString(outStr)
+		load := getLoad()
 
-		if err := t.Execute(buf, getLoad()); err == nil {
+		if err := t.Execute(buf, load); err == nil {
+			if loadVal, err := strconv.ParseFloat(load.Avg1, 64); err == nil {
+				if loadVal >= float64(c.UrgentLoad) {
+					outputBlock.Color = c.UrgentColor
+					outputBlock.Urgent = true
+				}
+			}
 			outputBlock.FullText = buf.String()
 		} else {
 			outputBlock.FullText = fmt.Sprint(err)
