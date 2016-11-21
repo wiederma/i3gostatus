ae7184116d4590c49e7dc0ecb92bfdcdabc7634c
Stefan Tatschner <rumpelsepp@sevenbyte.org>
2016-11-21 13:43:13 +0100

    Allocate a fresh block
    Otherwise urgentTemp does not get cleaned up...

--- lib/modules/temperature/temperature.go
+++ lib/modules/temperature/temperature.go
@@ -38,11 +38,12 @@ func (c *Config) ParseConfig(configTree *toml.TomlTree) {
 }
 
 func (c *Config) Run(args *model.ModuleArgs) {
-	outputBlock := model.NewBlock(moduleName, c.BaseConfig, args.Index)
 	thermalFile := "/sys/class/thermal/thermal_zone0/temp"
 	var temperatureStr string
 
 	for range time.NewTicker(c.Period).C {
+		outputBlock := model.NewBlock(moduleName, c.BaseConfig, args.Index)
+
 		if data, err := ioutil.ReadFile(thermalFile); err == nil {
 			temperatureStr = strings.TrimSuffix(strings.TrimSpace(string(data)), "000")
 		} else {
