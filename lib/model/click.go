7c4c7583a9b38da74c23e1344f1c7aad70195b4c
Stefan Tatschner <rumpelsepp@sevenbyte.org>
2016-12-04 16:18:39 +0100

    Add new infrastructure for click handlers
    - A click handler is now a function.
    - Click handlers should be placed in a separate file, click.go
    - Functions for click handling must be defined, the function pointers
      must then be placed in a ClickHandlers struct. This struct provides a
      method `NewListener`, which is used to spawn a listener as a go
      routine.
    - Optional data can be passed using the optional argument, interface{}.
    
    Here is an example for a click.go file:
    
            package pulseaudio
    
            import (
                    "fmt"
                    "github.com/rumpelsepp/i3gostatus/lib/model"
            )
    
            var clickHandlers *model.ClickHandlers = &model.ClickHandlers{
                    HandleRightClick: onRightClick,
                    HandleWheelUp:    onWheelUp,
                    HandleWheelDown:  onWheelDown,
            }
    
            func onRightClick(args *model.ModuleArgs, block *model.I3BarBlockWrapper, data interface{}) {
                    toggleMute()
    
                    // TODO: Remove duplicated code
                    if isMuted() {
                            block.Color = "#FF0000"
                    } else {
                            // Use standard color; unsetting it let the JSON
                            // marshaller omit the relevant field.
                            block.Color = ""
                    }
    
                    args.ClickEventCh <- block
            }
    
            func onWheelUp(args *model.ModuleArgs, block *model.I3BarBlockWrapper, data interface{}) {
                    if err := increaseVolume(5); err != nil {
                            return
                    }
    
                    config := data.(*Config)
                    block.FullText = fmt.Sprintf(config.Format, getCurrentVolume())
                    args.ClickEventCh <- block
            }
    
            func onWheelDown(args *model.ModuleArgs, block *model.I3BarBlockWrapper, data interface{}) {
                    if err := decreaseVolume(5); err != nil {
                            return
                    }
    
                    config := data.(*Config)
                    block.FullText = fmt.Sprintf(config.Format, getCurrentVolume())
                    args.ClickEventCh <- block
            }
    
    The click handler can be spawned using:
    
            go clickHandlers.NewListener(args, outputBlock)

---
+++ lib/model/click.go
@@ -0,0 +1,38 @@
+package model
+
+type ClickHandlers struct {
+	HandleLeftClick   func(*ModuleArgs, *I3BarBlockWrapper, interface{})
+	HandleMiddleClick func(*ModuleArgs, *I3BarBlockWrapper, interface{})
+	HandleRightClick  func(*ModuleArgs, *I3BarBlockWrapper, interface{})
+	HandleWheelUp     func(*ModuleArgs, *I3BarBlockWrapper, interface{})
+	HandleWheelDown   func(*ModuleArgs, *I3BarBlockWrapper, interface{})
+}
+
+func (handlers *ClickHandlers) NewListener(args *ModuleArgs, block *I3BarBlockWrapper, data ...interface{}) {
+	for event := range args.InCh {
+		switch event.Button {
+		case MouseButtonLeft:
+			if handlers.HandleLeftClick != nil {
+				handlers.HandleLeftClick(args, block, data)
+			}
+		case MouseButtonMiddle:
+			if handlers.HandleMiddleClick != nil {
+				handlers.HandleMiddleClick(args, block, data)
+			}
+		case MouseButtonRight:
+			if handlers.HandleRightClick != nil {
+				handlers.HandleRightClick(args, block, data)
+			}
+		case MouseWheelUp:
+			if handlers.HandleWheelUp != nil {
+				handlers.HandleWheelUp(args, block, data)
+			}
+		case MouseWheelDown:
+			if handlers.HandleWheelDown != nil {
+				handlers.HandleWheelDown(args, block, data)
+			}
+		default:
+			continue
+		}
+	}
+}
